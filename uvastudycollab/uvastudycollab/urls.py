ac8c79d6e833b7a1f840797dfbf86f8817221ac2
Jordan Lu <jjl4mb@virginia.edu>
2015-03-02 19:13:09 -0500

    static web page start

--- uvastudycollab/uvastudycollab/urls.py
+++ uvastudycollab/uvastudycollab/urls.py
@@ -1,10 +1,6 @@
-from django.conf.urls import patterns, include, url
-from django.contrib import admin
-
+from django.conf.urls import patterns, url
+from django.views.generic import TemplateView
+		
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'uvastudycollab.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
-    url(r'^admin/', include(admin.site.urls)),
+   url('^$', TemplateView.as_view(template_name='index.html')),
 )
