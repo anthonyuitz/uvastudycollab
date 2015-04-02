6f4b7ec702a63dacba9eddfbe10392c2c6593a16
Anthony Uitz <aru5mf@virginia.edu>
2015-04-02 14:43:57 -0400

    Added sign up sheet

--- uvastudycollab/uvastudycollab/urls.py
+++ uvastudycollab/uvastudycollab/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import patterns, url
 from django.views.generic import TemplateView
-		
+from studycollab import views
+
 urlpatterns = patterns('',
-   url('^$', TemplateView.as_view(template_name='index.html')),
-)
+   url('^$', TemplateView.as_view(template_name='index.html'), name='index'),
+   url(r'^register/$', views.register, name='register'),
+   )
