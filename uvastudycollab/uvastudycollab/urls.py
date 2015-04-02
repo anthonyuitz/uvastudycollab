8ee87a7dba9612142c5fbb459c44e96ec8a8f949
Anthony Uitz <aru5mf@virginia.edu>
2015-04-02 14:56:26 -0400

    fixed sign up to link back to index and added admin functionality

--- uvastudycollab/uvastudycollab/urls.py
+++ uvastudycollab/uvastudycollab/urls.py
@@ -1,8 +1,10 @@
-from django.conf.urls import patterns, url
+from django.conf.urls import patterns, include, url
 from django.views.generic import TemplateView
 from studycollab import views
+from django.contrib import admin
 
 urlpatterns = patterns('',
    url('^$', TemplateView.as_view(template_name='index.html'), name='index'),
    url(r'^register/$', views.register, name='register'),
+   url(r'^admin/', include(admin.site.urls)),
    )
